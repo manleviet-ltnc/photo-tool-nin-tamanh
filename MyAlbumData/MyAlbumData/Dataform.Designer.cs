ca26a380b36d03c0f2d86279a739f6836358177b
Bùi Thị Nin <ninbui0512@gmail.com>
2017-11-15 22:09:17 -0800

    Photo Tool 10

---
+++ MyAlbumData/MyAlbumData/Dataform.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MyAlbumData
+{
+    partial class Dataform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbAlbum = new System.Windows.Forms.ComboBox();
+            this.gridPhotoAlbum = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPhotoAlbum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Album:";
+            // 
+            // cmbAlbum
+            // 
+            this.cmbAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbAlbum.DropDownWidth = 500;
+            this.cmbAlbum.FormattingEnabled = true;
+            this.cmbAlbum.Location = new System.Drawing.Point(59, 10);
+            this.cmbAlbum.Name = "cmbAlbum";
+            this.cmbAlbum.Size = new System.Drawing.Size(229, 22);
+            this.cmbAlbum.TabIndex = 1;
+            this.cmbAlbum.SelectionChangeCommitted += new System.EventHandler(this.cmbAlbum_SelectionChangeCommitted);
+            // 
+            // gridPhotoAlbum
+            // 
+            this.gridPhotoAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridPhotoAlbum.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPhotoAlbum.Location = new System.Drawing.Point(16, 43);
+            this.gridPhotoAlbum.Name = "gridPhotoAlbum";
+            this.gridPhotoAlbum.Size = new System.Drawing.Size(406, 177);
+            this.gridPhotoAlbum.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(347, 226);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Dataform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.gridPhotoAlbum);
+            this.Controls.Add(this.cmbAlbum);
+            this.Controls.Add(this.label1);
+            this.Name = "Dataform";
+            this.Text = "MyAlbumData";
+            ((System.ComponentModel.ISupportInitialize)(this.gridPhotoAlbum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbAlbum;
+        private System.Windows.Forms.DataGridView gridPhotoAlbum;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ MyAlbumData/MyAlbumData/Dataform.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Manning.MyPhotoAlbum;
+using Manning.MyPhotoControls;
+
+namespace MyAlbumData
+{
+    public partial class Dataform : Form
+    {
+        private AlbumManager _manager;
+        internal AlbumManager Manager
+        {
+            get { return _manager; }
+        }
+
+        public Dataform()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            Version ver = new Version(Application.ProductVersion);
+            Text = String.Format("MyAlbumData {0:#}.{1:#}", ver.Major, ver.Minor);
+
+            _manager = new AlbumManager();
+
+            cmbAlbum.DataSource = Directory.GetFiles(AlbumManager.DefaultPath, "*.abm");
+            OpenAlbum();
+            SetDataSources();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmbAlbum_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            OpenAlbum();
+            SetDataSources();
+        }
+
+        private void OpenAlbum()
+        {
+            string albumPath = cmbAlbum.SelectedItem.ToString();
+            if (Manager.FullName == albumPath)
+                return;
+            if (CloseAlbum() == false)
+                return;
+            try
+            {
+                _manager = new AlbumManager(albumPath);
+            }
+            catch (AlbumStorageException aex)
+            {
+                _manager = new AlbumManager();
+                MessageBox.Show("Unable to open album - " + aex.Message);
+            }
+
+        }
+        private bool CloseAlbum()
+        {
+            if (Manager.Album != null)
+                Manager.Album.Dispose();
+
+            return true;
+        }
+
+        private void SetDataSources()
+        {
+            gridPhotoAlbum.DataSource = Manager.Album;
+        }
+        private void SetupGrid()
+        {
+            gridPhotoAlbum.SuspendLayout();
+            gridPhotoAlbum.AutoGenerateColumns = false;
+            gridPhotoAlbum.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+            DataGridViewImageColumn thumbCol = new DataGridViewImageColumn();
+            thumbCol.DataPropertyName = "Image";
+            thumbCol.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            thumbCol.Name = "Image";
+            thumbCol.Width = 50;
+
+            DataGridViewColumn captionCol = new DataGridViewTextBoxColumn();
+            captionCol.DataPropertyName = "Caption";
+            captionCol.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            captionCol.Name = "Caption";
+
+            DataGridViewColumn takenCol = new DataGridViewTextBoxColumn();
+            takenCol.DataPropertyName = "DateTaken";
+            takenCol.Name = "Date Taken";
+
+            DataGridViewColumn pgCol = new DataGridViewTextBoxColumn();
+            pgCol.DataPropertyName = "Photographer";
+            pgCol.Name = "Date Taken";
+
+            DataGridViewTextBoxColumn fileCol = new DataGridViewTextBoxColumn();
+            fileCol.DataPropertyName = "FileName";
+            fileCol.Name = "File Name";
+
+            gridPhotoAlbum.Columns.AddRange(new DataGridViewColumn[] {
+                thumbCol,
+                captionCol,
+                takenCol,
+                pgCol,
+                fileCol});
+
+            gridPhotoAlbum.ResumeLayout();
+
+        }
+    }
+}
